b180ce845754fa47634686d898a593cdfc71f3c5
Karen <panteat@uci.edu>
2016-06-06 10:39:32 -0700

    HMC5883L sensor updated!

--- Power Supply/Power Monitor for Sensors/HMC5883L.ino
+++ Power Supply/Power Monitor for Sensors/HMC5883L.ino
@@ -2,9 +2,12 @@
 
 //Data Output Rate: 75Hz
 //Normal Measurement mode
-//Gain: 1370
+//Gain: 1370 = +/- 0.88 Ga
 // 01111100 : ConfigRegA
 // 00000000 : ConfigRegB
+
+//Value of scale found here: bildr.org/2012/02/hmc5883l_arduino/
+float scale = 0.73;
 int configRegA = 0x00;
 int configRegB = 0x01;
 int modeReg = 0x2;
@@ -36,20 +39,19 @@ void setConfiguration(){
 
   
 void readData(){
-  int x_MSB = 0;
-  int x_LSB = 0;
-  int y_MSB = 0;
-  int y_LSB = 0;
-  int z_MSB = 0;
-  int z_LSB = 0;
+  int16_t x_MSB = 0;
+  int16_t x_LSB = 0;
+  int16_t y_MSB = 0;
+  int16_t y_LSB = 0;
+  int16_t z_MSB = 0;
+  int16_t z_LSB = 0;
   Wire.beginTransmission(hmc5883l);
   Wire.write(dataX_MSB);
 
   Wire.requestFrom(hmc5883l,6);
-  Serial.print("Number of available bits: ");
+  //Serial.print("Number of available bits: ");
   int available = Wire.available();
   if(6<= available){
-    Serial.println(available);
     x_MSB = Wire.read() << 8;
     x_LSB = Wire.read();
     y_MSB = Wire.read() << 8;
@@ -57,15 +59,30 @@ void readData(){
     z_MSB = Wire.read() << 8;
     z_LSB = Wire.read();
   }
+  float x_raw = (x_MSB | x_LSB);
+  float y_raw = (y_MSB | y_LSB);
+  float z_raw = (z_MSB | z_LSB);
   Serial.print("x: ");
-  Serial.println(x_MSB | x_LSB);
+  Serial.println(x_raw);
   Serial.print("y: ");
-  Serial.println(y_MSB | y_LSB);
+  Serial.println(y_raw);
   Serial.print("z: ");
-  Serial.println(z_MSB | z_LSB);
+  Serial.println(z_raw);
+
+  float heading = atan2(y_raw, x_raw);
+
+  if(heading < 0){
+    heading += 2 * PI;
+  }
+  else if(heading > 2*PI){
+    heading -= 2*PI;
+  }
+  float headingDegrees = heading * 180/M_PI;
+  Serial.print("heading: ");
+  Serial.println(headingDegrees);
   Wire.endTransmission(); 
 
-  delay(250);
+  delay(200);
 
 
 
